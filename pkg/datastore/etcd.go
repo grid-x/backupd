ad0b150605368f20818083b2ff08bc18781f8739
Joel Hermanns <joel.hermanns@gmail.com>
2017-05-15 13:00:59 +0200

    feat(datastore): add support for etcd datastore

---
+++ pkg/datastore/etcd.go
@@ -0,0 +1,61 @@
+package datastore
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"time"
+
+	"github.com/coreos/etcd/client"
+	"github.com/mickep76/etcdmap"
+)
+
+type Etcd struct {
+	endpoint string
+}
+
+func NewEtcd(endpoint string) *Etcd {
+	return &Etcd{
+		endpoint: endpoint,
+	}
+}
+
+func (e *Etcd) ExportTo(tmpdir string) error {
+	cfg := client.Config{
+		Endpoints: []string{e.endpoint},
+		Transport: client.DefaultTransport,
+		// set timeout per request to fail fast when the target endpoint is unavailable
+		HeaderTimeoutPerRequest: time.Second,
+	}
+	c, err := client.New(cfg)
+	if err != nil {
+		return err
+	}
+
+	kapi := client.NewKeysAPI(c)
+	// set "/foo" key with "bar" value
+	resp, err := kapi.Get(context.Background(), "/", &client.GetOptions{Recursive: true})
+	if err != nil {
+		return err
+	}
+	// Export and write output.
+	m := etcdmap.Map(resp.Node)
+
+	// TODO: add time stamp here
+	f, err := ioutil.TempFile(tmpdir, "etcd-")
+	if err != nil {
+		return err
+	}
+
+	j, err := json.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	_, err = f.Write(j)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
